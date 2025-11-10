77953835ff8b6680f427205faeadaa690484a609
pucher220121 <pucher220121@sr.htlweiz.at>
2025-11-10 08:04:12 +0100

    a1

--- A1_ZweiThreadsZaehlenWinner/Program.cs
+++ A1_ZweiThreadsZaehlenWinner/Program.cs
@@ -9,18 +9,23 @@ class Program
     
     public static void Main(string[] args)
     {
-        Console.WriteLine("Übung 1: Zwei Threads – Zählen & Winner");
-        
-        
+        Thread countUp = new Thread(CountUpThreadA);
+        Thread countDown = new Thread(CountDownThreadB);
+
+        countUp.Start();
+        countDown.Start();
+
+        countUp.Join();
+        countDown.Join();
     }
     
     private static void CountUpThreadA()
     {
-        
+        for (int i = 0; i <= 100; i++) { Console.WriteLine(i);  Thread.Sleep(100); };
     }
     
     private static void CountDownThreadB()
     {
-       
+       for (int i = 100; i >= 0; i--) { Console.WriteLine(i); Thread.Sleep(100); };
     }
 }
