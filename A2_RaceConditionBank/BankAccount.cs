e9de3b1cd96d72dba561a2714ace7c68c96b294d
pucher220121 <pucher220121@sr.htlweiz.at>
2025-11-10 08:46:01 +0100

    a2_01

--- A2_RaceConditionBank/BankAccount.cs
+++ A2_RaceConditionBank/BankAccount.cs
@@ -13,13 +13,13 @@ public class BankAccount
     }
     
     public void Deposit(int amount) 
-    { 
-       
+    {
+        balance += amount;
     }
     
     public void Withdraw(int amount) 
-    { 
-        
+    {
+        balance -= amount;
     }
     
     public int GetBalance() 
--- A2_RaceConditionBank/Program.cs
+++ A2_RaceConditionBank/Program.cs
@@ -5,20 +5,37 @@ namespace A2_RaceConditionBank;
 
 class Program
 {
+    //static Object _lock = new Object();
     public static void Main(string[] args)
     {
+        List<Thread> threads = new List<Thread>();
         Console.WriteLine("Übung 2: Race Condition – Bankkonto");
         Console.WriteLine("==========================================\n");
         
         // Bankkonto mit Startwert 1000 EUR erstellen
         BankAccount account = new BankAccount(1000);
         Console.WriteLine($"Startkontostand: {account.GetBalance()} EUR\n");
-        
+        Thread t1;
+
+        for (int i = 0; i < 10; i++)
+        {
+            t1 = new Thread(() => PerformBankOperations(account));
+            threads.Add(t1);
+            t1.Start();
+        }
+        foreach(Thread t in threads)
+        {
+            t.Join();
+        }
+        Console.WriteLine($"Endkontostand: {account.GetBalance()} EUR\n");
     }
     
     private static void PerformBankOperations(BankAccount account)
     {
-        
+        account.Deposit(100);
+        Thread.Sleep(100);
+        account.Withdraw(150);
+        Thread.Sleep(100);
     }
 }
 
